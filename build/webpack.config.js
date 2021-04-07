c8de8ba1fbfdb0654b588dd0fb77863b592b1397
陈乃华 <810422646@qq.com>
2021-04-07 18:25:31 +0800

    no message

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,16 +1,26 @@
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.scss$|\.sass$/,
-        use: [{
-          loader: 'sass-loader',
-          options: {
-            sourceMap: false,
-            additionalData: `@import "src/assets/styles/variable.scss";`
-          }
-        }]
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
+module.exports = (config, options) => {
+  config.module.rules.push({
+    test: /\.scss$|\.sass$/,
+    use: [{
+      loader: 'sass-loader',
+      options: {
+        sourceMap: false,
+        additionalData: `@import "src/assets/styles/variable.scss";`
       }
-    ]
+    }]
+  })
+
+  if (options && options.optimization) {
+    config.plugins.push(
+      new CompressionWebpackPlugin({
+        test: /\.(js|css)$/,
+        filename: '[path][base].gz',
+        algorithm: 'gzip',
+        threshold: 10240,
+        minRatio: 0.8
+      })
+    )
   }
 }
